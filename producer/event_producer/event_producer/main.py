afc785f51a0ab0d372519761811bc5411ba4b37d
Usman Ashraf <45431758+usmanashrf@users.noreply.github.com>
2024-05-18 12:53:40 +0500

    Update main.py

--- producer/event_producer/event_producer/main.py
+++ producer/event_producer/event_producer/main.py
@@ -2,6 +2,17 @@ from contextlib import asynccontextmanager
 from time import sleep 
 from json import dumps
 from fastapi import FastAPI
+from sqlmodel import SQLModel
+from typing import Optional
+from aiokafka import AIOKafkaProducer
+
+
+class RideRequest(SQLModel):
+    id: Optional[int]
+    client_name:str
+    location :str
+    destination :str
+    fear : int
 
 
 
@@ -21,7 +32,19 @@ app = FastAPI(lifespan = lifespan, title="Event producer"
 
 @app.get("/")
 def root():
-    return {"message": "Hello from producer"}
+    return {"message": "Hello from class"}
 
+@app.post("/create_request")
+async def create_request():
+    producer = AIOKafkaProducer(bootstrap_servers='broker:19092')
+    # Get cluster layout and initial topic/partition leadership information
+    await producer.start()
+    try:
+        # Produce message
+        await producer.send_and_wait("ride", b"book ride for me")
+    finally:
+        # Wait for all pending messages to be delivered or expire.
+        await producer.stop()
 
+    return {"this","is request","function"}
 
